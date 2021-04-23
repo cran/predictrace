dc7da1f7389819a100dd1a5300f97a7beb1bf070
Jacob Kaplan <jkkaplan6@gmail.com>
2021-04-23 18:50:06 +0000

    version 2.0.0

--- inst/doc/Predict-race-of-surname.R
+++ inst/doc/Predict-race-of-surname.R
@@ -7,6 +7,18 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(predictrace)
 
+## ---- eval = FALSE------------------------------------------------------------
+#  To install this package, use the code
+#  install.packages("predictrace")
+#  
+#  
+#  # The development version is available on Github.
+#  # install.packages("devtools")
+#  devtools::install_github("jacobkap/predictrace")
+
+## -----------------------------------------------------------------------------
+library(predictrace)
+
 ## -----------------------------------------------------------------------------
 predict_race("Washington")
 
@@ -15,4 +27,25 @@ predict_race(c("Washington", "Franklin", "Lincoln"))
 
 ## -----------------------------------------------------------------------------
 predict_race("Washington", probability = FALSE)
+
+## -----------------------------------------------------------------------------
+predict_race("sarah", surname = FALSE)
+
+## -----------------------------------------------------------------------------
+predict_race(c("sarah", "jaime", "jon", "bao"), surname = FALSE)
+
+## -----------------------------------------------------------------------------
+predict_race("sarah", probability = FALSE, surname = FALSE)
+
+## -----------------------------------------------------------------------------
+predict_gender("tyrion")
+
+## -----------------------------------------------------------------------------
+predict_gender(c("tyrion", "jaime", "jon", "sansa", "arya", "cersei"))
+
+## -----------------------------------------------------------------------------
+predict_gender(c("tyrion", "jaime", "jon", "sansa", "arya", "cersei"), probability = FALSE)
+
+## -----------------------------------------------------------------------------
+predict_gender("zzztyrion")
 
